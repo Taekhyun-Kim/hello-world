fe442751d4af5b4321922443cb2a0a7873a41bab
Kim <Kim@TaekHyun>
2019-08-13 18:10:20 +0900

    print 메세지 변경

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 	public static void main(String[] orgs) {
 		System.out.println("안녕하세요");
-		System.out.println("Java를 처음 공부해봤어요!");
+		System.out.println("Java 프로그래밍 시작!");
 	}
 }
